c70db49f9c7e98fdbacad2ca912f132ecbbd175c
KatIsCoding <fabrigoru@gmail.com>
2021-12-15 08:37:27 -0600

    Problem solution

--- morse_code.rb
+++ morse_code.rb
@@ -8,3 +8,27 @@ def decode_char(code_char)
   index = code.find_index(code_char)
   alphabeth[index]
 end
+
+
+def decode_word(word)
+  word = word.split(' ')
+  out = ""
+  word.each {|char| 
+    if char == '/'
+      out += " "  
+    else
+      out += decode_char(char)
+    end
+  }
+  return out
+end
+
+def decode(sentence)
+  sentence = sentence.split('   ')
+  out = ""
+  sentence.each {|word| out += decode_word(word) + " "}
+  return out
+end
+
+print decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...")
+
