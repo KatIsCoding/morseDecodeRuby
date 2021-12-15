3d53c9be36a0c1632467fa6fa09729a05e4ed147
KatIsCoding <fabrigoru@gmail.com>
2021-12-15 08:38:47 -0600

    Linter corrections

--- morse_code.rb
+++ morse_code.rb
@@ -9,26 +9,24 @@ def decode_char(code_char)
   alphabeth[index]
 end
 
-
 def decode_word(word)
-  word = word.split(' ')
-  out = ""
-  word.each {|char| 
-    if char == '/'
-      out += " "  
-    else
-      out += decode_char(char)
-    end
-  }
-  return out
+  word = word.split
+  out = ''
+  word.each do |char|
+    out += if char == '/'
+             ' '
+           else
+             decode_char(char)
+           end
+  end
+  out
 end
 
 def decode(sentence)
   sentence = sentence.split('   ')
-  out = ""
-  sentence.each {|word| out += decode_word(word) + " "}
-  return out
+  out = ''
+  sentence.each { |word| out += "#{decode_word(word)} " }
+  out
 end
 
-print decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...")
-
+print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
